91b53464ca530f3b5d639737bb393415be13a27f
nafryer97 <nafryer97@ou.edu>
2019-04-21 15:46:43 -0500

    constructed a table. designing grading algorithm.

--- hw6.py
+++ hw6.py
@@ -1,5 +1,62 @@
 with open("input.txt") as file:
-    data = file.read()
+    input = file.read()
 
-digits = filter(str.isdigit, data)
-print digits
+data = input.split()
+
+table = []
+
+for x in range(0, len(data)):
+    if len(data[x]) == 9 and data[x].isdigit():
+        table.append(data[x:x+5])
+        
+print "\nTable:\n"
+
+for x in table:
+    print x
+
+print "\nSorted:\n"
+
+table.sort(key=lambda x: x[-2], reverse=True)
+
+for x in table:
+    print x
+
+grades = []
+
+a = 0
+b = 0
+
+for x in range(0, len(table)):
+    grades.append(table[x][0:3])
+    if x < (len(table) / 3):
+        if a > (len(table) / 3) and grades[x - 1][3] == "A":
+            grades[x].append("B")
+        else:
+            grades[x].append("A")
+            a += 1
+    elif x < (2 *(len(table) / 2)):
+        if b > (2 * (len(table) / 2)) and grades[x -1][3] == "B":
+            if table[x - 1][-1] == "E":
+                
+            else:    
+                grades[x].append("C")
+    elif x >= ((len(table) % 10) - 1):
+        grades[x].append("F")
+    else:
+        grades[x].append("C")
+
+print "\nGrades:\n"
+
+for x in grades:
+    print x
+
+print "\nCounts:\n"
+print a
+print b
+
+    
+
+
+
+
+
